ad710620cb3d2f47cd7d30c0e324cda9b57ba81e
Alila kevin <alilakevin4@gmail.com>
2024-10-22 17:20:11 +0300

    employment system

--- class.py
+++ class.py
@@ -1,30 +1,25 @@
-class student:
-    def __init__(self):
-        self.name = "James Ngali"
-        self.adm = "T5457U"
-        self.idno = 3456890
-        self.year =2019
-        self.lap = self.laptop()
-    
+class employee:
+    def __init__(self, name, idno):
+        self.name=name
+        self.idno=idno
     def show(self):
-        print(self.name, self.adm, self.idno, self.year)
-        self.lap.show()
-    
-    class laptop:
-        def __init__(self):
-            self.ram ='8 GB'
-            self.cpu ='i5'
-            self.brand ='HP/DELL/Lenevo'
-        def show(self):
-            print(self.ram, self.cpu, self.brand)
-class almin(student):
-    def __init__(self, name, adm, idno, year):
-        super().__init__(name, adm, idno, year)
-        self.fy = 2024
+        return f"name: {self.name}, id number {self.idno}"
+class manager(employee):
+    def __init__(self, name, idno, depart):
+        super().__init__(name, idno)
+        self.depart = depart
     def show(self):
-        print(self.fy)
-        return self.super().show()           
-
-#s1=student("Trend anode", "35343R", 53535353, 2019)
-a1=almin
-a1.show()
+        return super().show() + f"manager of {self.depart}"
+class intern(employee):
+    def __init__(self, name, idno, period):
+        super().__init__(name, idno)
+        self.period = period
+    def show(self):
+        return super().show() + f"is here for {self.period} months"
+    
+employees=[
+    manager("James nandi", 2745365, "finance"),
+    intern("George Njoroge", 3790876, 6)
+]
+for emp in employees:
+    print(emp.show())
